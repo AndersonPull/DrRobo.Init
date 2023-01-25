f99f9e8f45b4bee0037a7d39325c43f378a88ef9
Anderson Oliveira <anderson.oliveira@vitreo.com.br>
2023-01-25 12:29:56 -0300

    Merge branch 'main' of https://github.com/AndersonPull/DrRobo.Init

--- Modules/RemotelyControlled/Models/DroneModel.cs
+++ Modules/RemotelyControlled/Models/DroneModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Drrobo.Modules.Shared.Models;
+
 namespace Drrobo.Modules.RemotelyControlled.Models
 {
 	public class DroneModel : BaseModel
---
+++ Utils/Constant/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Drrobo.Modules.RemotelyControlled.Services;
+using Refit;
+
+namespace Drrobo.Utils.Constant
+{
+    public partial struct Constants
+    {
+        public static readonly IRemotelyControlledService BaseServeLocal = RestService.For<IRemotelyControlledService>("web-service-gerado-pelo-esp32");
+    }
+}
