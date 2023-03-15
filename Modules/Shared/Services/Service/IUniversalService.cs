3fd3202e8540f90cfe43b596c33c83b5c7baec0a
Anderson Oliveira Bezerra <andersonoliveera@outlook.com>
2023-03-15 07:56:13 -0300

    Create IUniversalService.cs

---
+++ Modules/Shared/Services/Service/IUniversalService.cs
@@ -0,0 +1,13 @@
+﻿using Refit;
+
+namespace Drrobo.Modules.Shared.Services.Service
+{
+	public interface IUniversalService
+	{
+        [Post("")]
+        Task<string> Request(string request);
+
+        [Post("drrobo")]
+        Task<string> RequestDrRobo(string request);
+    }
+}
