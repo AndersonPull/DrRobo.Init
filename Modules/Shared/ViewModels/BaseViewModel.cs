71a2e671c4d07fe8c4523effb281eb71e9b32fd9
Anderson Oliveira Bezerra <andersonoliveera@outlook.com>
2023-03-15 07:56:16 -0300

    Update BaseViewModel.cs

--- Modules/Shared/ViewModels/BaseViewModel.cs
+++ Modules/Shared/ViewModels/BaseViewModel.cs
@@ -1,30 +1,56 @@
-﻿using GalaSoft.MvvmLight;
+﻿using Drrobo.Modules.Shared.Enums;
+using Drrobo.Modules.Shared.Services.Service;
+using Drrobo.Utils.Bluetooth;
+using Drrobo.Utils.Constant;
+using GalaSoft.MvvmLight;
+using Plugin.BLE.Abstractions.Contracts;
 using PropertyChanged;
 
 namespace Drrobo.Modules.Shared.ViewModels
 {
     public class BaseViewModel : ViewModelBase
     {
-        public  BaseViewModel()
-        {
-        }
+        public  BaseViewModel(){}
 
         public virtual Task InitializeAsync(object navigationData)
-        {
-            return Task.FromResult(false);
-        }
-
-        private bool _isBusy;
-        public bool IsBusy { get { return _isBusy; } set { Set("IsBusy", ref _isBusy, value); } }
+            => Task.FromResult(false);
     }
     
-
     [AddINotifyPropertyChangedInterface]
     public class BaseViewModel<T> : BaseViewModel
     {
+        public BaseViewModel()
+        {
+        }
+
         private T _model = Activator.CreateInstance<T>();
         public T Model { get { return _model; } set { Set("Model", ref _model, value); } }
+
+        private bool _isBusy;
+        public bool IsBusy { get { return _isBusy; } set { Set("IsBusy", ref _isBusy, value); } }
+
+
+        public async Task Communication(CommunicationEnum communication, string request, IUniversalService universalService = null, IBluetoothUtil bluetoothUtil = null, IDevice device = null)
+        {
+            try
+            {
+                switch (communication)
+                {
+                    case CommunicationEnum.Wifi:
+                        universalService.Request(request).GetAwaiter().GetResult();
+                        break;
+                    case CommunicationEnum.WifiRobo:
+                        universalService.RequestDrRobo(request).GetAwaiter().GetResult();
+                        break;
+                    case CommunicationEnum.Bluetooth:
+                        bluetoothUtil.SendAsync(device, request).GetAwaiter().GetResult();
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                await Application.Current.MainPage.DisplayAlert("Erro na comunicação", "Certifique que as configurações estão corretas", "OK");
+            }
+        }
     }
 }
-
-
