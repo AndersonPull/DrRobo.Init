2f08fd93fd3df3de424cd8fcefca80d1924506b1
Anderson Oliveira Bezerra <andersonoliveera@outlook.com>
2023-04-03 00:05:01 -0300

    buttonArrow

---
+++ Modules/Shared/Components/Buttons/ButtonArrowComponent.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows.Input;
+
+namespace Drrobo.Modules.Shared.Components.Buttons;
+
+public partial class ButtonArrowComponent : ContentView
+{
+    public static readonly BindableProperty TextComponentProperty = BindableProperty.Create(nameof(TextComponent), typeof(string), typeof(ButtonArrowComponent));
+    public static readonly BindableProperty SubTextComponentProperty = BindableProperty.Create(nameof(SubTextComponent), typeof(string), typeof(ButtonArrowComponent));
+    public static readonly BindableProperty CommandProperty = BindableProperty.Create(nameof(Command), typeof(ICommand), typeof(ButtonArrowComponent));
+    public static readonly BindableProperty CommandParameterProperty = BindableProperty.Create(nameof(CommandParameter), typeof(object), typeof(ButtonArrowComponent));
+
+    public ButtonArrowComponent()
+    {
+        InitializeComponent();
+    }
+
+    public string TextComponent
+    {
+        get => (string)GetValue(TextComponentProperty);
+        set => SetValue(TextComponentProperty, value);
+    }
+
+    public string SubTextComponent
+    {
+        get => (string)GetValue(SubTextComponentProperty);
+        set => SetValue(SubTextComponentProperty, value);
+    }
+
+    public ICommand Command
+    {
+        get => (ICommand)GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
+    }
+
+    public object CommandParameter
+    {
+        get => GetValue(CommandParameterProperty);
+        set => SetValue(CommandParameterProperty, value);
+    }
+}
