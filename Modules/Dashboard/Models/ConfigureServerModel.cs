9f1b70edf546399bc5f7e888e8c48200afad8bd2
Anderson Oliveira Bezerra <andersonoliveera@outlook.com>
2023-04-09 10:36:43 -0300

    adicionando swipeview para servers

---
+++ Modules/Dashboard/Models/ConfigureServerModel.cs
@@ -0,0 +1,15 @@
+﻿using Drrobo.Modules.Shared.Models;
+
+namespace Drrobo.Modules.Dashboard.Models
+{
+	public class ConfigureServerModel
+	{
+        public ConfigureServerModel()
+        {
+            ServerList = new List<ServerModel>();
+        }
+
+        public List<ServerModel> ServerList { get; set; }
+	}
+}
+
--- Modules/Dashboard/ViewModels/ConfigureServerViewModel.cs
+++ Modules/Dashboard/ViewModels/ConfigureServerViewModel.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 using CommunityToolkit.Maui.Views;
+using Drrobo.Modules.Dashboard.Models;
 using Drrobo.Modules.Shared.Components.PopUp;
 using Drrobo.Modules.Shared.Models;
 using Drrobo.Modules.Shared.Services.Navigation;
@@ -8,7 +8,7 @@ using Drrobo.Modules.Shared.ViewModels;
 
 namespace Drrobo.Modules.Dashboard.ViewModels
 {
-	public class ConfigureServerViewModel : BaseViewModel<BaseModel>
+	public class ConfigureServerViewModel : BaseViewModel<ConfigureServerModel>
     {
         public ICommand AddServerCommand => new Command(async () => await AddServerAsync());
 
@@ -17,6 +17,15 @@ namespace Drrobo.Modules.Dashboard.ViewModels
         public ConfigureServerViewModel(INavigationService serviceNavigation)
         {
             _serviceNavigation = serviceNavigation;
+
+            var List = new List<ServerModel>()
+            {
+                new ServerModel(){ Name= "Robo1", URL="https://teste"},
+                 new ServerModel(){ Name= "Robo2", URL="https://teste"},
+                  new ServerModel(){ Name= "Robo3", URL="https://teste"}
+            };
+
+            Model.ServerList = List;
         }
 
         private async Task AddServerAsync()
