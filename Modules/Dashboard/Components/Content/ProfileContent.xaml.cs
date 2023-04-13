20991702cfa1fe0abccace5f0047989c6b468bd6
Anderson Oliveira Bezerra <andersonoliveera@outlook.com>
2023-04-13 09:49:05 -0300

    Update ProfileContent.xaml.cs

--- Modules/Dashboard/Components/Content/ProfileContent.xaml.cs
+++ Modules/Dashboard/Components/Content/ProfileContent.xaml.cs
@@ -1,9 +1,36 @@
-﻿namespace Drrobo.Modules.Dashboard.Components.Content;
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace Drrobo.Modules.Dashboard.Components.Content;
 
 public partial class ProfileContent : ContentView
 {
-	public ProfileContent()
-	{
-		InitializeComponent();
-	}
+    public ProfileContent()
+    {
+        InitializeComponent();
+
+        SetDeviceDisplay();
+        DeviceDisplay.MainDisplayInfoChanged += DeviceDisplay_MainDisplayInfoChanged;
+    }
+
+    private void DeviceDisplay_MainDisplayInfoChanged(object sender, DisplayInfoChangedEventArgs e)
+        => SetDeviceDisplay();
+    
+    private void SetDeviceDisplay()
+    {
+        if (DeviceInfo.Idiom == DeviceIdiom.Desktop)
+            return;
+
+        switch (DeviceDisplay.Current.MainDisplayInfo.Orientation)
+        {
+            case DisplayOrientation.Landscape:
+                if (DeviceInfo.Platform == DevicePlatform.iOS)
+                    ProfileStackLayout.Margin = new Thickness(47);
+                break;
+            case DisplayOrientation.Portrait:
+                if (DeviceInfo.Platform == DevicePlatform.iOS)
+                    ProfileStackLayout.Margin = new Thickness(15, 45, 15, 15);
+                break;
+        }
+    }
 }
