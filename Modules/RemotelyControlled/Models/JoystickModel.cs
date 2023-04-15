4d8ce52204f886400a33c251c1ffa992f12177e0
Anderson Oliveira Bezerra <andersonoliveera@outlook.com>
2023-04-15 18:38:35 -0300

    Create JoystickModel.cs

---
+++ Modules/RemotelyControlled/Models/JoystickModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.ObjectModel;
+using Drrobo.Modules.Shared.Models;
+
+namespace Drrobo.Modules.RemotelyControlled.Models
+{
+	public class JoystickModel : BaseModel
+    {
+        public BluetoothModel Bluetooth { get; set; } = new BluetoothModel();
+    }
+}
