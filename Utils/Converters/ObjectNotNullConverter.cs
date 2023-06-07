88e5633b415b4049f1686423e3df6041a9a65a2d
AndersonPull <andersonoliveera166@gmail.com>
2023-06-07 19:43:05 -0300

    Adicionando o priveiro conversor

---
+++ Utils/Converters/ObjectNotNullConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Drrobo.Utils.Converters
+{
+	public class ObjectNotNullConverter
+	{
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value != null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
