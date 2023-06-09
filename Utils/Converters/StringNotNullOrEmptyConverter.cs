5546d83efd0a19f7fb92b3b8c35fab032c07f0dd
AndersonPull <andersonoliveera166@gmail.com>
2023-06-09 15:28:05 -0300

    Create StringNotNullOrEmptyConverter.cs

---
+++ Utils/Converters/StringNotNullOrEmptyConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Drrobo.Utils.Converters
+{
+    public class StringNotNullOrEmptyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !string.IsNullOrEmpty(value?.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
