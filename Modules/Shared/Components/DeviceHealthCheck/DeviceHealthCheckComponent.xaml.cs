8a834b26f762bd36365b238bfdbd0bab312cb0b4
Anderson Oliveira Bezerra <andersonoliveera@outlook.com>
2023-07-12 22:45:33 -0300

    criacao do component health check

---
+++ Modules/Shared/Components/DeviceHealthCheck/DeviceHealthCheckComponent.xaml.cs
@@ -0,0 +1,36 @@
+﻿using LiveChartsCore;
+using LiveChartsCore.Measure;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using SkiaSharp;
+
+namespace Drrobo.Modules.Shared.Components.DeviceHealthCheck;
+
+public partial class DeviceHealthCheckComponent : ContentView
+{
+    public static readonly BindableProperty BatteryPercentageProperty = BindableProperty.Create(nameof(BatteryPercentage), typeof(IEnumerable<ISeries>), typeof(DeviceHealthCheckComponent));
+
+    public DeviceHealthCheckComponent()
+    {
+        InitializeComponent();
+
+        seting();
+    }
+
+    public IEnumerable<ISeries> BatteryPercentage
+    {
+        get => (IEnumerable<ISeries>)GetValue(BatteryPercentageProperty);
+        set => SetValue(BatteryPercentageProperty, value);
+    }
+
+    private void seting()
+    {
+        BatteryPercentage = new GaugeBuilder()
+            .WithInnerRadius(120)
+            .WithBackgroundInnerRadius(120)
+            .WithBackground(new SolidColorPaint(new SKColor(30, 30, 30)))
+            .WithLabelsPosition(PolarLabelsPosition.ChartCenter)
+            .AddValue(75, null, new SKColor(47, 193, 44), SKColors.Transparent)
+            .BuildSeries();
+    }
+}
