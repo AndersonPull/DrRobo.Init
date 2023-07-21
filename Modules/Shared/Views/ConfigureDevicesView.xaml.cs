677003bd9f0abc51d48c1d4a51f61cdfda14babf
Anderson Oliveira Bezerra <andersonoliveera@outlook.com>
2023-07-21 15:45:00 -0300

    Update ConfigureDevicesView.xaml.cs

--- Modules/Shared/Views/ConfigureDevicesView.xaml.cs
+++ Modules/Shared/Views/ConfigureDevicesView.xaml.cs
@@ -1,33 +1,9 @@
-﻿using Drrobo.Utils;
-
-namespace Drrobo.Modules.Shared.Views;
+﻿namespace Drrobo.Modules.Shared.Views;
 
 public partial class ConfigureDevicesView : ContentPage
 {
 	public ConfigureDevicesView()
 	{
 		InitializeComponent();
-
-        NameEntry.TextChanged += OnEntryTextChanged;
-    }
-
-    private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
-    {
-        if (!string.IsNullOrEmpty(NameEntry.Text))
-        {
-            SaveButton.IsEnabled = true;
-            SaveButton.Style = Util.GetResource<Style>("PrimaryButton");
-
-            UpdateButton.IsEnabled = true;
-            UpdateButton.Style = Util.GetResource<Style>("PrimaryButton");
-        }
-        else
-        {
-            SaveButton.IsEnabled = false;
-            SaveButton.Style = Util.GetResource<Style>("DisableButton");
-
-            UpdateButton.IsEnabled = false;
-            UpdateButton.Style = Util.GetResource<Style>("DisableButton");
-        }
     }
 }
