4f57976920ff2455d58521a334b394d3f4830e3e
Anderson Oliveira Bezerra <andersonoliveera@outlook.com>
2023-07-25 15:08:25 -0300

    animation cam

---
+++ Modules/RemotelyControlled/Components/AnimationCam.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace Drrobo.Modules.RemotelyControlled.Components;
+
+public partial class AnimationCam : ContentView
+{
+	public AnimationCam()
+	{
+		InitializeComponent();
+        _ = StartAnimationJoystick();
+    }
+
+    private async Task StartAnimationJoystick()
+    {
+        await Task.Delay(500);
+        await DeviceImage.RotateTo(-90, 700);
+        await Task.Delay(500);
+    }
+}
