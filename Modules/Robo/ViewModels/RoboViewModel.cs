a9e9127895f09e845db7234c519c58fad4e669f0
Anderson Oliveira Bezerra <andersonoliveera@outlook.com>
2024-03-20 20:57:24 -0300

    teste de resposta com voz

--- Modules/Robo/ViewModels/RoboViewModel.cs
+++ Modules/Robo/ViewModels/RoboViewModel.cs
@@ -1,17 +1,13 @@
-﻿using CommunityToolkit.Maui.Alerts;
-using CommunityToolkit.Maui.Media;
+﻿using CommunityToolkit.Maui.Media;
 using Drrobo.Modules.Robo.Models;
 using Drrobo.Modules.Shared.Services.Navigation;
 using Drrobo.Modules.Shared.ViewModels;
 using System.Globalization;
-using System.Windows.Input;
 
 namespace Drrobo.Modules.Robo.ViewModels
 {
     class RoboViewModel : BaseViewModel<RoboModel>
     {
-        public ICommand ListenCommand => new Command(async () => await ListenAsync());
-
         INavigationService _serviceNavigation;
         private readonly ISpeechToText _speechToText;
         public RoboViewModel(INavigationService serviceNavigation, ISpeechToText speechToText)
@@ -20,30 +16,39 @@ namespace Drrobo.Modules.Robo.ViewModels
             _speechToText = speechToText;
         }
 
+        public override async Task InitializeAsync(object navigationData)
+        {
+            await ListenAsync();
+            await base.InitializeAsync(navigationData);
+        }
+
         private async Task ListenAsync()
         {
             var tokenSource = new CancellationTokenSource();
             if (!await _speechToText.RequestPermissions(tokenSource.Token))
             {
-                await Toast.Make("Permission not granted").Show(CancellationToken.None);
+                await Application.Current.MainPage.DisplayAlert("Error", "Permission not granted", "OK");
                 return;
             }
 
-            Model.RecognitionText = string.Empty;
-            var recognitionResult = await _speechToText.ListenAsync(
-                                                CultureInfo.GetCultureInfo("pt-br"),
-                                                new Progress<string>(partialText =>
-                                                {
-                                                    Model.RecognitionText += partialText + " ";
-                                                }), tokenSource.Token);
+            var recognitionResult = await _speechToText
+                .ListenAsync(
+                    CultureInfo.GetCultureInfo("pt-br"),
+                    new Progress<string>(async partialText => await ActionAsync(partialText)),
+                    tokenSource.Token);
 
-            if (recognitionResult.IsSuccessful)
-                Model.RecognitionText = recognitionResult.Text;
-            else
-            {
-                await Toast.Make(recognitionResult.Exception?.Message ?? "Unable to recognize speech").Show(CancellationToken.None);
+            if (!recognitionResult.IsSuccessful)
                 await Application.Current.MainPage.DisplayAlert("Error", recognitionResult.Exception?.Message, "OK");
-            }
+        }
+
+        public async Task ActionAsync(string value)
+        {
+            if (value.Contains("horas"))
+                Model.RecognitionText = $"São {DateTime.Now.ToString("HH:mm")}";
+            else
+                Model.RecognitionText = $"Desculpe, não entendi o que você falou";
+
+            await TextToSpeech.Default.SpeakAsync(Model.RecognitionText);
         }
     }
 }
